31d3ac3df9ced63ea8e72442911dbe642913328a
C-Tharun <116745984+C-Tharun@users.noreply.github.com>
2025-05-12 09:22:30 +0530

    .

--- campaignify-xenocrm/src/app/dashboard/page.tsx
+++ campaignify-xenocrm/src/app/dashboard/page.tsx
@@ -31,7 +31,7 @@ export default async function DashboardPage() {
       orderBy: { createdAt: "desc" },
       include: {
         _count: {
-          select: { customers: true },
+          select: { customerToSegments: true },
         },
       },
     }),
